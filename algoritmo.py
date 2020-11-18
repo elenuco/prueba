ed4328f072eb2e7f8b2d23e1b5e92a23041d393f
Evelyn Campos <elenuco36@gmail.com>
2020-11-18 09:42:44 -0600

    modulos de node y cambios en index y algoritmo.

--- algoritmo.py
+++ algoritmo.py
@@ -2,12 +2,28 @@ import itertools
 import collections
 
 # My Simple implementation of Levenshtein distance
-def levenshtein_distance(string1, string2):
+def levenshteinD(string1, string2):
    """
->>> levenshtein_distance("AATZ", "AAAZ")
+>>> levenshteinD("AATZ", "AAAZ")
 1
->>> levenshtein_distance("AATZZZ", "AAAZ")
+>>> levenshteinD("AATZZZ", "AAAZ")
 3
    """
+   m = len(string1)
+   n = len(string2)
+   table = [[0] * (n + 1) for _ in range(m + 1)]
 
-distance = 0
+   for i in range(m + 1):
+       table[i][0] = i
+   for j in range(n + 1):
+        table[0][j] = j
+
+   for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            if string1[i - 1] == string2[j - 1]:
+                table[i][j] = table[i - 1][j - 1]
+            else:
+                table[i][j] = 1 + min(table[i - 1][j], table[i][j - 1], table[i - 1][j - 1])
+
+   return table[-1][-1]
+levenshteinD("risk", "think")
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const{ spawn } = require('child_process');
-const childPython = spawn('python', ['algoritmo.py', 'str1', 'str2']);
-let salida=[];
-childPython.stdout.on('data', (data)=>{
-    console.log(`stdout:${data}`);
-});
-childPython.stderr.on('data', (data)=>{
-   console.error(`stdout:${data}`);
-});
-childPython.on('close', callback=>{
-    console.info(salida.toString())
-});
+const express = require("express")
+const app= express()
+app.get('/', (req, res, next)=>{
+    next()
+})
+app.get('/', (req, res, next)=>{
+    console.log("Hola Mundo")
+})
+app.listen('3000', ()=>{
+    console.log("Servidor en 3000")
+})
+//const{ spawn } = require('child_process');
+//const childPython = spawn('python', ['algoritmo.py', 'str1', 'str2']);
+//let salida=[];
+//childPython.stdout.on('data', (data)=>{
+    //console.log(`stdout:${data}`);
+//});
+//childPython.stderr.on('data', (data)=>{
+  // console.error(`stdout:${data}`);
+//});
+//childPython.on('close', callback=>{
+   // console.info(salida.toString())
+//});
